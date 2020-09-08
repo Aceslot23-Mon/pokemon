9dc7b84f33f6323156c1829d2f9b4b6b51a22264
Aceslot23-Mon <aceslot08@gmail.com>
2020-09-08 22:54:20 +0200

    aggiunto dist

---
+++ build/precache-manifest.619409c3a2deb0ede0b8da2701f949a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5195b852ea0879b266cab263b56ddc48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652e4705bd9203ea0bfc",
+    "url": "/static/css/main.3b3e8d70.chunk.css"
+  },
+  {
+    "revision": "bd5429e6e3aef2767913",
+    "url": "/static/js/2.5aadebbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5aadebbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652e4705bd9203ea0bfc",
+    "url": "/static/js/main.584e801d.chunk.js"
+  },
+  {
+    "revision": "965aca300182a94c14f3",
+    "url": "/static/js/runtime-main.4126d0af.js"
+  },
+  {
+    "revision": "ee784ca07479b1f8b5550c74c8c7e2f9",
+    "url": "/static/media/Poke_Ball.ee784ca0.webp"
+  },
+  {
+    "revision": "0f2fbe5c99665e64d10cbe92faf2069a",
+    "url": "/static/media/Pokeball.0f2fbe5c.png"
+  },
+  {
+    "revision": "edd72be48d41170cc2ee61a6c8f46657",
+    "url": "/static/media/pngegg.edd72be4.png"
+  }
+]);
